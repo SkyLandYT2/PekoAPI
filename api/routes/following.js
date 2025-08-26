7c822c2f1b359d99027b8b587fc94323e55f839e
SkyLandYT2 <83461291+SkyLandYT2@users.noreply.github.com>
2025-08-26 18:59:24 +0300

    work pls

--- api/routes/following.js
+++ api/routes/following.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { getUserFollowing } = require("../controllers/followingController");
 
-// GET /api/user/following?id={userid}&page={page}
-router.get("/user/following", getUserFollowing);
+router.get("/", getUserFollowing);
 
 module.exports = router;
